c07bf3c6b803a4318de888671bbc18cf6d655234
Jaehee Chung <brillar2225@gmail.com>
2023-06-11 02:57:25 +0900

    ADD: Add footer contents and its styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function Footer() {
-  return <footer>&copy; Calendory {new Date().getFullYear()}</footer>;
+  return (
+    <footer className='flex justify-center items-center border-t border-zinc-300 shadow-inner'>
+      <div className='max-w-xs w-full sm:max-w-sm'>
+        <h4 className='my-1 text-center'>
+          Copyright &copy; {new Date().getFullYear()}{' '}
+          <a href='https://github.com/brillar2225/' className='underline'>
+            Jaehee Chung
+          </a>{' '}
+          Reserved.
+        </h4>
+        <div className='flex justify-between my-2 w-full border-t border-neutral-500'>
+          <h5 className='basis-3/12 font-semibold text-left'>Contact</h5>
+          <p className='basis-9/12 text-right'>brillar2225&#64;gmail.com</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
